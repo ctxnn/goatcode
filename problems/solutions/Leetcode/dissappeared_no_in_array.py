4bf58d73380e393703a9c3d21f16005cd169c0b4
ctxnn <taneja.chirag.ct@gmail.com>
2026-08-06 19:20:27 +0530

    chore: move personal solution code to private goatcode-solutions repo

--- problems/solutions/Leetcode/dissappeared_no_in_array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        result = []
-        l = set(nums)
-        for i in range(1,len(nums)+1):
-            if i not in l: 
-                result.append(i)
-        return result
--- problems/solutions/Leetcode/minimum_time_visiting_all_points.py
+++
@@ -1,6 +0,0 @@
-total = 0
-for i in range(len(points) - 1):
-    dx = abs(points[i+1][0] - points[i][0])
-    dy = abs(points[i+1][1] - points[i][1])
-    total += max(dx, dy)
-return total
